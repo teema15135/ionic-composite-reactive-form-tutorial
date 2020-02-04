970bc59eb87b0c3b2b92a7410fb675231cb2b6e5
Teema Luksameset <teema.ma.luk@kkumail.com>
2020-02-04 21:48:27 +0700

    complete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  myFG: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.myFG = this.formBuilder.group({
+      foodName: [''],
+      foodPrice: [''],
+      description: [''],
+      options: this.formBuilder.array([
+        this.formBuilder.group({
+          optionTitle: [''],
+          choices: this.formBuilder.array([
+            this.formBuilder.group({
+              choiceTitle: [''],
+              choicePrice: ['']
+            })
+          ])
+        })
+      ])
+    });
+  }
+
+  getOptions() {
+    return this.myFG.get('options') as FormArray;
+  }
+
+  getChoices(optionIndex: number) {
+    return this.getOptions().at(optionIndex).get('choices') as FormArray;
+  }
+
+  addOption() {
+    this.getOptions().push(
+      this.formBuilder.group({
+        optionTitle: [''],
+        choices: this.formBuilder.array([
+          this.formBuilder.group({
+            choiceTitle: [''],
+            choicePrice: ['']
+          })
+        ])
+      })
+    );
+  }
+
+  addChoice(optionIndex: number) {
+    this.getChoices(optionIndex).push(
+      this.formBuilder.group({
+        choiceTitle: [''],
+        choicePrice: ['']
+      })
+    );
+  }
+
+  logNgoNgo() {
+    console.log(this.myFG.value);
+  }
 
 }
